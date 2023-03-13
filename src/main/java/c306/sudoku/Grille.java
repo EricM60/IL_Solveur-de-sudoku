3f115cdfd4dfc0330640ba4387d59822e69aeb08
Lodraie <Loick.herman@orange.Fr>
2023-03-13 16:02:10 +0100

    Maj reduc error Checkstyle 2

--- src/main/java/c306/sudoku/Grille.java
+++ src/main/java/c306/sudoku/Grille.java
@@ -9,11 +9,11 @@ import c306.exception.ValeurImpossibleException;
 import c306.exception.ValeurInitialeModificationException;
 
 /**
- * Interface de grille de sudoku. Chaque case d'une Grille peut contenir un ElementDeGrille 
- * ou null si aucun élément n'est placé.
+ * Interface de grille de sudoku.
+ *  Chaque case d'une Grille peut contenir un ElementDeGrille
+ *  ou null si aucun élément n'est placé.
  * Une Grille doit toujours respecter les règles du sudoku.
- * <p>
- * Une Grille peut contenir des cases qui ne doivent pas être modifiées 
+ * Une Grille peut contenir des cases qui ne doivent pas être modifiées
  * (les valeurs initiales de la Grille)
  *
  * @author Sébastien Choplin <sebastien.choplin@u-picardie.fr>
@@ -21,9 +21,7 @@ import c306.exception.ValeurInitialeModificationException;
 public interface Grille {
 
     /**
-     * Renvoie les ElementDeGrille pouvant exister dans le grille.
-     * 
-     * @return
+     * @return les ElementDeGrille pouvant exister dans le grille.
      */
     Set<ElementDeGrille> getElements();
 
@@ -33,21 +31,28 @@ public interface Grille {
     int getDimension();
 
     /**
-     * Affecte une valeur dans une case de la grille, ou null pour 'vider' la case.
+     * Affecte une valeur dans une case de la grille,
+     *  ou null pour 'vider' la case.
      *
      * @param x     position x dans la grille
      * @param y     position y dans la grille
-     * @param value élément de grille à mettre dans la case, null pour vider la case
-     * @throws ValeurImpossibleException           si l'élément de grille n'est pas
-     *  autorisé à cette position dans la grille aux vues des autres valeurs de la grille
-     * @throws ElementInterditException            si l'élément de grille n'est pas
-     *  autorisé dans cette grille pouvant être mis dans la grille
-     * @throws HorsBornesException                 si x ou y sont en dehors de la grille
-     * @throws ValeurInitialeModificationException si une valeur initiale de la grille 
-     * est en position x,y
+     * @param value élément de grille à mettre dans la case,
+     *  null pour vider la case
+     * @throws ValeurImpossibleException
+     * si l'élément de grille n'est pas autorisé
+     *  à cette position dans la grille aux vues des autres valeurs de la grille
+     * @throws ElementInterditException
+     * si l'élément de grille n'est pas autorisé 
+     *  dans cette grille pouvant être mis dans la grille
+     * @throws HorsBornesException
+     * si x ou y sont en dehors de la grille
+     * @throws ValeurInitialeModificationException
+     * si une valeur initiale de la grille 
+     *  est en position x,y
      */
     void setValue(int x, int y, ElementDeGrille value)
-            throws HorsBornesException, ValeurImpossibleException, ElementInterditException, ValeurInitialeModificationException;
+            throws HorsBornesException, ValeurImpossibleException, 
+            ElementInterditException, ValeurInitialeModificationException;
 
     /**
      * Renvoie une valeur de la grille.
